66d36d958aeac15e6616259f792fc1b12dd81f9f
tilast <tilast.lab@gmail.com>
2014-06-14 16:47:19 +0300

    added logo

--- public/js/main.js
+++ public/js/main.js
@@ -5,5 +5,9 @@ $(document).ready(function() {
   // 	$(".accordion-toggle").addClass('collapsed');
   // 	$(".accordion-body").style("height", "0px");
   // });
+	var header_bgs = ['/img/header_bg.jpg', '/img/header_bg_2.jpg'];
+	setInterval(function() {
+		
+	}, 1000);
 
 });
